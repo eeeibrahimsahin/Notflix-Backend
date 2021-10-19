f6a16bdc955f66d3224ae72315a7490f86246ec9
eeeibrahimsahin <56609130+eeeibrahimsahin@users.noreply.github.com>
2021-10-19 06:58:47 +0200

    Initial commit

---
+++ src/main/java/com/notflix/notflixbackend/NotflixBackendApplication.java
@@ -0,0 +1,13 @@
+package com.notflix.notflixbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotflixBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotflixBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/notflix/notflixbackend/NotflixBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.notflix.notflixbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotflixBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
